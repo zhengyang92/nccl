4828143f158be3459ea4a0bc086003626af14d59
Zhengyang Liu <liuz@cs.utah.edu>
2021-02-08 08:10:54 +0000

    stage

--- src/collectives/device/all_gather.h
+++ src/collectives/device/all_gather.h
@@ -41,16 +41,16 @@ class ncclFunction<ncclFuncAllGather, NCCL_ALGO_RING, NCCL_PROTO_SIMPLE, FUNC, T
         ncclPrimitives<UNROLL, 1, 1, T, 0, 1, 1, FUNC>
           prims(tid, nthreads, NULL, &nghr, thisOutput, 1, channel, comm, ncclShmem->ptrs, 0);
 
-        for (int step = 0; step < NSTEPS; step++){
+        for (int step = 0; step < 2; step++){
           int curSchedule1 = schedule[step][myRank][sid * 2];
           int curSchedule2 = schedule[step][myRank][sid * 2 + 1];
-          if (curSchedule1 != -1){
+          if (curSchedule1 != -1) {
             if (curSchedule1 >= (myRank + 1) * 6 || curSchedule1 < myRank * 6)
-              while (signals[curSchedule1] != 1) {}
+              while (*(signals+curSchedule1) != 1) {}
           }
-          if (curSchedule2 != -1){
+          if (curSchedule2 != -1) {
             if (curSchedule2 >= (myRank + 1) * 6 || curSchedule2 < myRank * 6)
-              while (signals[curSchedule2] != 1) {}
+              while (*(signals+curSchedule2) != 1) {}
           }
 
           if (curSchedule1 != -1){
@@ -61,23 +61,21 @@ class ncclFunction<ncclFuncAllGather, NCCL_ALGO_RING, NCCL_PROTO_SIMPLE, FUNC, T
           if (curSchedule2 != -1){
             prims.directSend(thisOutput + curSchedule2 * size, curSchedule2 * size, size);
           }
-          __syncthreads();
-          __threadfence_system();
         }
       } else {
         int m1 = -1;
         ncclPrimitives<UNROLL, 1, 1, T, 1, 1, 1, FUNC>
             prims(tid, nthreads, &nghr, &m1, thisOutput, 1, channel, comm, ncclShmem->ptrs, 0);
-        for (int step = 0; step < NSTEPS; step++){
-          int curSchedule1 = schedule[step][myRank][sid * 2];
-          int curSchedule2 = schedule[step][myRank][sid * 2 + 1];
+        for (int step = 0; step < 2; step++){
+          int curSchedule1 = schedule[step][myRank][sid * 2 + 2*NNBGRS];
+          int curSchedule2 = schedule[step][myRank][sid * 2 + 1 + 2*NNBGRS];
           if (curSchedule1 != -1){
             prims.directRecv(thisOutput + curSchedule1 * size, curSchedule1 * size, size);
           }
           if (curSchedule2 != -1){
             prims.directRecv(thisOutput + curSchedule2 * size, curSchedule2 * size, size);
           }
-
+          __syncthreads();
           if (tid == 0){
             if (curSchedule1 != -1){
               signals[curSchedule1] = 1;
